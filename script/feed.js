7d9e6a18cc186675a9564bb3359fbffea19d7361
Alvin Lin <omgimanerd@yahoo.com>
2014-08-10 14:41:54 -0400

    Updated feed scripts

--- script/feed.js
+++ script/feed.js
@@ -20,12 +20,8 @@ function appendToFeed(header, text) {
 
   headerRow.appendChild(headerColumn);
   textRow.appendChild(textColumn);
-  feedTable.appendChild(headerRow);
-  feedTable.appendChild(textRow);
+  feedTable.insertBefore(textRow, feedTable.firstChild);
+  feedTable.insertBefore(headerRow, feedTable.firstChild);
 }
 
 appendToFeed('Introducing our New Website', 'FTC is not just about building and programming a robot, and it\'s not just about standing in a lab and working reallylate on the night before competition. It\'s also aboutspreading the joy and excitement of STEM to communityaround us. With this website, we hope to show why we love robotics to everyone in any community, in the hope thatwe inspire kids of all ages to go out and make something.');
-
-appendToFeed('Hi dere','tehasoidfha;weo');
-
-appendToFeed('First post','Lorem ipsum dolor sit amet asdfiha;sdich;aowiehf;asdifj;a tesl;iahc;kuhawef');
--- script/feed.py
+++ script/feed.py
@@ -1,17 +1,17 @@
-#!/usr/bin/python
+import time;
 
 feedjs = open('feed.js', 'a');
 
-print "Gimme a header:";
+print 'Gimme a header:';
 header = raw_input();
-print "Type your entire post:";
+print 'Type your entire post:';
 text = raw_input();
 
-func = '\nappendToFeed(\''+header+'\',\''+text+'\');';
+func = '\nappendToFeed(\''+header+' - '+time.strftime("%m/%d/%Y")+'\',\''+text+'\');';
 
 feedjs.write(func);
 feedjs.write('\n');
 feedjs.close();
 
-print "Post successfully appended";
+print 'Post successfully appended ' + time.strftime("%m/%d/%Y");
 end = raw_input();
