d1ec257caf0162ce2f884a68ba5a2a0be8275f8c
Alvin Lin <omgimanerd@yahoo.com>
2014-08-10 10:57:23 -0700

    Feed scripting

---
+++ script/feed.js
@@ -0,0 +1,14 @@
+/**
+ * This file will be modified by a python script to add new updates to the
+ * feed.
+ * @author: Alvin Lin (alvin.lin@stuypulse)
+ */
+
+function appendToFeed(header, text) {
+  document.getElementById('homepage-content-feed-table');
+  var tableRow = document.createElement('tr');
+  var header = document.createElement('td');
+  tableData.setAttribute('class', 'homepage-content-feed-header');
+  var text = document.create
+
+}
