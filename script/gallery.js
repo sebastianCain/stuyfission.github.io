4c8a991f133fabf3971d31304ec8614f86d45322
Alvin Lin <omgimanerd@yahoo.com>
2014-08-16 00:18:19 -0700

    Removed ID tagging from pictures script

--- script/gallery.js
+++ script/gallery.js
@@ -4,46 +4,64 @@
  * @author Alvin Lin (alvin.lin@stuypulse.com)
  */
 
-function Gallery (objectName, thumbnailEl, displayEl) {
-  this.objectName_ = objectName;
-  this.thumbnailParent_ = thumbnailEl;
-  this.displayParent_ = displayEl;
+/**
+ * @constructor
+ * @param {Element} parentEl
+ * @param {string} objectName
+ */
+function Gallery(parentEl, objectName) {
 
-  this.thumbnailContainer_ = createDiv('gallery-thumbnails');
-  this.displayContainer_ = createDiv('gallery-display');
+  this.parentEl_ = parentEl;
+  this.objectName_ = objectName;
+
   this.pictures_ = [];
 };
 
-Gallery.prototype.addPicture = function (url) {
+/**
+ * Adds pictures to this gallery object.
+ * @param {string} url URL of the picture
+ * @param {string} caption
+ */
+Gallery.prototype.addPicture = function(url, caption) {
   var thumbnail = createImg(url);
   var displayPicture = createImg(url);
+
   this.pictures_.push(
-      {'thumbnail':thumbnail,
-       'displayPicture':displayPicture});
+      {'thumbnail' : thumbnail,
+       'displayPicture' : displayPicture});
 };
 
+/**
+ * Builds the gallery and appends it to the parent element.
+ */
 Gallery.prototype.build = function () {
+  var thumbnailContainer = createDiv('gallery-thumbnails');
+  var displayContainer = createDiv('gallery-display');
   for (var i = 0; i < this.pictures_.length; i++) {
     // Sets an onclick property for each thumbnail that will call
     // the changeImage function
     this.pictures_[i].thumbnail.setAttribute(
-        'onclick', this.objectName_+'.changeImage('+i.toString()+')');
-    this.thumbnailContainer_.appendChild(this.pictures_[i].thumbnail);
+        'onclick',
+        this.objectName_+'.changeImage('+i.toString()+')');
+    thumbnailContainer.appendChild(this.pictures_[i].thumbnail);
 
-    this.pictures_[i].displayPicture.setAttribute(
-        'id', 'displayPicture'+i.toString());
+    // Sets the displays of the pictures to 'none'.
     this.pictures_[i].displayPicture.style.display = 'none';
-    this.displayContainer_ .appendChild(this.pictures_[i].displayPicture);
+    displayContainer.appendChild(this.pictures_[i].displayPicture);
   }
-  this.thumbnailParent_.appendChild(this.thumbnailContainer_);
-  this.displayParent_.appendChild(this.displayContainer_);
+
+  this.parentEl_.appendChild(thumbnailContainer);
+  this.parentEl_.appendChild(displayContainer);
 };
 
+/**
+ * @private
+ */
 Gallery.prototype.changeImage = function (index) {
 	for (var i = 0; i < this.pictures_.length; i++) {
-    document.getElementById('displayPicture'+i.toString()).style.display = 'none';
+    this.pictures_[i].displayPicture.style.display = 'none';
 	}
-  document.getElementById('displayPicture'+index.toString()).style.display = 'block';
+  this.pictures_[index].displayPicture.style.display = 'block';
 };
 
 Gallery.prototype.size = function() {
