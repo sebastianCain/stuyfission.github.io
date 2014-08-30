e83bb52019eddba6c1e6e718ad518aad5f462db0
Alvin Lin <omgimanerd@yahoo.com>
2014-08-29 23:18:15 -0400

    Finished basic restructuring, styling nits remaining

--- script/bio-tabs.js
+++ script/bio-tabs.js
@@ -7,19 +7,19 @@
  * @param {string} selectedSection
  */
 function setSelected(selectedTab) {
-  var tabs = document.getElementById('team-tabs')
+  var tabs = document.getElementById('team-content-tabs')
              .getElementsByTagName('a');
   for (var i = 0; i < tabs.length; i++) {
     tabs[i].setAttribute('class', 'unselected');
   }
-  var selectedTabEl = document.getElementById('team-tabs-'+selectedTab);
+  var selectedTabEl = document.getElementById('team-content-tabs-'+selectedTab);
   selectedTabEl.setAttribute('class', 'selected');
 
-  var sections = document.getElementById('team-tabs-content')
+  var sections = document.getElementById('team-content-text')
                  .getElementsByTagName('div');
   for (var i = 0; i < sections.length; i++) {
     sections[i].setAttribute('class', 'hidden');
   }
-  var selectedSectionEl = document.getElementById('team-tabs-content-'+selectedTab);
+  var selectedSectionEl = document.getElementById('team-content-text-'+selectedTab);
   selectedSectionEl.setAttribute('class', 'small visible');
 };
