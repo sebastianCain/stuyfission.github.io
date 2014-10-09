de8d68a417ca82ff83bdef52ea0b73f7a574a07e
Alvin Lin <alvin.lin@stuypulse.com>
2014-10-09 09:09:29 -0400

    Merged revamp branch

---
+++ script/tabs.js
@@ -0,0 +1,53 @@
+/**
+ * @author Alvin Lin (alvin.lin@stuypulse.com)
+ */
+
+/**
+ * @private
+ */
+function showSelected (tabContainerId, contentContainerId, index) {
+  var tabContainerEl = document.getElementById(tabContainerId);
+  var contentContainerEl = document.getElementById(contentContainerId);
+
+  // Store the tab and content elements in parallel arrays. The arrays
+  // must be parallel and equal in length (1 tab per section).
+  var tabEls = getImmediateChildren(
+      tabContainerEl, 'DIV');
+  var contentEls = getImmediateChildren(
+      contentContainerEl, 'DIV');
+
+  for (var i = 0; i < contentEls.length; ++i) {
+    if (i == index) {
+      tabEls[i].setAttribute('class', 'selected');
+      contentEls[i].setAttribute('class', 'selected');
+    } else {
+      tabEls[i].setAttribute('class', 'unselected');
+      contentEls[i].setAttribute('class', 'unselected');
+    }
+  }
+};
+
+/**
+ * This function applies tab switching behavior to the tabs.
+ * @param {string} tabContainerId
+ * @param {string} contentContainerId
+ */
+function applyClickEvents (tabContainerId, contentContainerId) {
+  // Check if the tabs have the same number of divs as the content divs.
+  var tabContainerEl = document.getElementById(tabContainerId);
+  var contentContainerEl = document.getElementById(contentContainerId);
+  var tabEls = getImmediateChildren(
+      tabContainerEl, 'DIV');
+  var contentEls = getImmediateChildren(
+      contentContainerEl, 'DIV');
+  if (tabEls.length !== contentEls.length) {
+    throw new Error("Unable to build tabs.");
+  }
+
+  // Set the onclick events.
+  for (var i = 0; i < tabEls.length; ++i) {
+    tabEls[i].setAttribute(
+        'onclick', 'showSelected("' + tabContainerId + '","' +
+        contentContainerId + '",' + i.toString() + ')');
+  }
+};
